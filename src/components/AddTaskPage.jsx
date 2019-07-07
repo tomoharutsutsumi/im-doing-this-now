44cbd6d113864effeca8d4be41325a71f851c2ae
堤智春 <tsutsumitomoharu@tsutsumichiharunoMacBook-Pro.local>
2019-07-07 17:37:07 +0900

    flow導入完了

--- src/components/AddTaskPage.jsx
+++ src/components/AddTaskPage.jsx
@@ -1,13 +1,17 @@
+// @flow
 import React from 'react';
 import { connect } from 'react-redux';
 import { addTask } from '../actions/tasks';
 import AddTaskForm from './AddTaskForm';
 
-const AddTaskPage = ({ dispatch, task }) => (
+type Props = {
+  dispatch: (e: Event) => void,
+}
+
+const AddTaskPage = ({ dispatch }: Props) => (
   <div>
     <p>Add Task</p>
     <AddTaskForm
-      task={task}
       onSubmit={(task) => {
         dispatch(addTask(task));
       }}
--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,13 +1,24 @@
+// @flow
 import React from 'react';
 
+type Props = {
+  requester: String,
+  worker: String,
+  startDate: Date,
+  deadlineDate: Date,
+  contents: String,
+  isFinished: boolean,
+  onClick: (e: Event) => void
+}
+
 export default ({
-  requester, worker, startDate, deadLineDate, contents, isFinished, onClick,
-}) => (
+  requester, worker, startDate, deadlineDate, contents, isFinished, onClick,
+}: Props) => (
   <div>
     <p>{requester}</p>
     <p>{worker}</p>
     <p>{startDate}</p>
-    <p>{deadLineDate}</p>
+    <p>{deadlineDate}</p>
     <p>{contents}</p>
     {!isFinished && <p onClick={onClick}>Finished</p>}
   </div>
